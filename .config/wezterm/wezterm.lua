5ea702fe3904cb1dc75abcc8acc78402cedce01c
dknelson9876 <dknelson9876@gmail.com>
2024-05-01 22:46:51 -0600

    wezterm - further dim inactive panes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "GruvboxDark"
 config.hide_tab_bar_if_only_one_tab = true
+config.font_size = 11
 
 -- Set background to an image that scales to the height of the window
 -- and sticks to the left
@@ -20,5 +21,8 @@ config.background = {
 	},
 }
 
-config.font_size = 11
+config.inactive_pane_hsb = {
+	saturation = 0.7,
+	brightness = 0.3,
+}
 return config
