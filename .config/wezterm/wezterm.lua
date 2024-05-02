8facec0c985ab3e0ab7c556bc439b6cb24e14747
dknelson9876 <dknelson9876@gmail.com>
2024-05-01 22:47:15 -0600

    wezterm - custom pane keybinds

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -25,4 +25,70 @@ config.inactive_pane_hsb = {
 	saturation = 0.7,
 	brightness = 0.3,
 }
+
+-- Configure Alt+arrow to move focus, Alt+Sh+arrow to adjust size, and Alt+Sh+w to close panes
+config.keys = {
+	{
+		key = "LeftArrow",
+		mods = "ALT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "RightArrow",
+		mods = "ALT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "DownArrow",
+		mods = "ALT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "UpArrow",
+		mods = "ALT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "LeftArrow",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.AdjustPaneSize({ "Left", 1 }),
+	},
+	{
+		key = "RightArrow",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.AdjustPaneSize({ "Right", 1 }),
+	},
+	{
+		key = "DownArrow",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.AdjustPaneSize({ "Down", 1 }),
+	},
+	{
+		key = "UpArrow",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.AdjustPaneSize({ "Up", 1 }),
+	},
+	{
+		key = "_",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "+",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "w",
+		mods = "ALT|SHIFT",
+		action = wezterm.action.CloseCurrentPane({ confirm = false }),
+	},
+}
+
 return config
