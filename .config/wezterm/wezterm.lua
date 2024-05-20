0720f012c9c5f0a169293817d4e551a15103bb05
dknelson9876 <dknelson9876@gmail.com>
2024-05-20 11:54:12 -0600

    feat: wezterm right click copy/paste

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -91,4 +91,22 @@ config.keys = {
 	},
 }
 
+local act = wezterm.action
+
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
+}
+
 return config
