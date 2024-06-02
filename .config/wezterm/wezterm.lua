863840dc5f2e1fa13164090e2c696ed4befe56fa
dknelson9876 <dknelson9876@gmail.com>
2024-06-01 23:14:06 -0600

    new: switch to kanagawa colors

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "GruvboxDark"
+config.color_scheme = "Kanagawa (Gogh)"
 config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 11
 
