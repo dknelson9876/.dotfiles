d920a505973929433771f3ca6d7c24b4379a6b23
dknelson9876 <dknelson9876@gmail.com>
2024-06-25 21:56:50 -0600

    new: add mini.align nvim plugin

--- .config/nvim/lua/dknelson/lazy/mini.lua
+++ .config/nvim/lua/dknelson/lazy/mini.lua
@@ -3,6 +3,7 @@ return {
     config = function()
         -- treesitter based text objects
         require("mini.ai").setup { n_lines = 500 }
+        require("mini.align").setup()
 
         -- TODO: Other mini plugins
     end,
